b901b2e37f5eaa548777a4021cca62e5e1b2973d
jasminvasquez <deysijasmin@unitec.com>
2014-03-18 23:55:40 -0600

    Update jav.java

--- src/jav.java
+++ src/jav.java
@@ -5,37 +5,37 @@ public class jav {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Scanner miscanner=new Scanner(System.in);
-		System.out.print("ingrese un numero");
-		int num1=miscanner.nextInt();
-		System.out.print("ingrese otro numero");
-		int num2=miscanner.nextInt();
-		System.out.print("ingrese 1 para +; 2 para -; 3 para *; 4 para /");
-		int num3=miscanner.nextInt();
-		{
-		if(num3==1)
-
-		System.out.print("la suma es:");
-		System.out.println(num1+num2);
-		}
-		{
-		if(num3==2)
-		System.out.print("la resta es:");
-		System.out.println(num1-num2);
-		}
-		{
-		if(num3==3)
-		System.out.print("la multiplicacion es:");
-		System.out.println(num1*num2);
-		}
-		{
-		if(num3==4)
-		System.out.print("la division es:");
-		System.out.println(num1/num2);
-		}
-
-
-		}
-		}
-
+                
+		int num1,num2,suma,resta,multi,division,n;
+		Scanner escaner = new Scanner (System.in);
+		System.out.print("Ingrese primer numero:"); 
+		num1=escaner.nextInt();
+		System.out.print("Ingrese segundo numero:"); 
+		num2=escaner.nextInt();
+		System.out.println("Mostrar Opcion:"); 
+		System.out.println("1:"); 
+		System.out.println("2:"); 
+		System.out.println("3:"); 
+		System.out.println("4:"); 
+		n=escaner.nextInt(); 
+		switch (n){ 
+		case 1:
+			suma=num1+num2; 
+			System.out.println("La suma es"+suma); 
+			break; 
+			case 2: 
+				resta=num1-num2;
+				System.out.println("La resta es"+resta);
+            break; 
+            case 3:
+            	multi=num1*num2; 
+            System.out.println("La multiplicacion es"+multi); 
+            break; 
+            case 4: 
+            	division=num1/num2; 
+            	System.out.println("La division es"+division);
+            	break; 
+            
+            	} 
+		} 
+	} 
